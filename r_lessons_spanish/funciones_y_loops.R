42f0dee960592a226d30d2a42dc4abab3028f16d
NicoleMarie-16 <nicolemdavila16@gmail.com>
2021-07-26 21:39:51 -0500

    adds script to teach functions, for loops, and lapply

---
+++ r_lessons_spanish/funciones_y_loops.R
@@ -0,0 +1,103 @@
+# Script info ---------------------------------------------------------------------------------------------
+# Script Name: funciones_y_loops.R 
+# Script Author: Nicole Davila
+# Created date: Mon Jul 26 20:26:53 2021
+# Purpose: Este codigo es para ense~ar lo que son las funciones, loops y el lapply
+
+### Import required libraries -----------------------------------------------------------------------------
+library(data.table)
+
+### Define functions --------------------------------------------------------------------------------------
+edad_en_dias <- function(nombre = character(),
+                        edad_en_anos = numeric()){
+  tryCatch(
+    expr = {
+      edad_dias <- edad_en_anos * 365
+      print(paste0(nombre, " tiene ", edad_dias, " dias de edad."))
+      resultados <- list(tabla = data.table(nombre = nombre, edad_anos = edad_en_anos, edad_dias = edad_dias, estatus = "EXITO", mensaje = NA_character_),
+                         dias_de_edad = edad_dias)
+      return(resultados)
+    }, error = function(e){
+      print(paste0("Ha ocurrido un error calculando la edad en dias: ", e$message))
+      resultados <- list(tabla = data.table(nombre = nombre, edad_anos = edad_en_anos, edad_dias = NA_real_, estatus = "ERROR", mensaje = e$message),
+                         dias_de_edad = NA_real_)
+      return(resultados)
+    }
+  )
+}
+
+### Define variables --------------------------------------------------------------------------------------
+anos <- c(1, 2, 3, 4, 5, 6)
+tabla_prueba <- data.table(nombre = c("Liann", "Nicole", "Jorge", "William"),
+                           edad = c(16, 25, 39, 28))
+
+tabla_error <- data.table(nombre = c("Liann", "Nicole", "Jorge", "William"),
+                          edad = list(16, 25, "39", "28"))
+
+### Load data ---------------------------------------------------------------------------------------------
+
+
+### Pre-process -------------------------------------------------------------------------------------------
+
+
+### Process -----------------------------------------------------------------------------------------------
+# Cómo se aplicaría esta función con sólo una observación?
+edad_en_dias(nombre = "Nicole", edad_en_anos = 25)
+
+# No siempre tienes que especificar los argumentos, luego de que esten en el orden correcto:
+edad_en_dias("Liann", 16)
+
+# Para quedarte con la tabla que te dice el estatus de la funcion puedes correr:
+tabla_resultados <- edad_en_dias(nombre = "Nicole", edad_en_anos = 25)$tabla
+tabla_resultados
+
+# Para quedarte con la edad en dias puedes correr:
+edad_dias <- edad_en_dias(nombre = "Nicole", edad_en_anos = 25)$dias_de_edad
+edad_dias
+
+# Que pasa si hay un error?
+edad_en_dias(nombre = "Liann", edad_en_anos = "17")
+
+# Como capturar el error?
+mensaje_error <- edad_en_dias(nombre = "Liann", edad_en_anos = "17")$tabla$mensaje
+mensaje_error
+
+# For loop
+for(ano in anos){
+  print(ano*365)
+}
+
+# Lapply
+# Hagamos que la funcion solo regrese una tabla con resultados en lugar de una lista
+edad_en_dias <- function(nombre = character(),
+                         edad_en_anos = numeric()){
+  tryCatch(
+    expr = {
+      edad_dias <- edad_en_anos * 365
+      print(paste0(nombre, " tiene ", edad_dias, " dias de edad."))
+      resultados <- data.table(nombre = nombre, edad_anos = edad_en_anos, edad_dias = edad_dias, estatus = "EXITO", mensaje = NA_character_)
+      return(resultados)
+    }, error = function(e){
+      print(paste0("Ha ocurrido un error calculando la edad en dias: ", e$message))
+      resultados <- data.table(nombre = nombre, edad_anos = edad_en_anos, edad_dias = NA_real_, estatus = "ERROR", mensaje = e$message)
+      return(resultados)
+    }
+  )
+}
+ 
+# Cuando no hay error tenemos los siguientes resultados
+resultados <- lapply(seq_along(tabla_prueba$nombre), function (x) edad_en_dias(nombre = tabla_prueba$nombre[[x]],
+                                                                               edad_en_anos = tabla_prueba$edad[[x]]))
+
+tabla_final <- rbindlist(resultados)
+
+# CUando ocurre un error tenemos los siguientes resuktados
+resultados2 <- lapply(seq_along(tabla_error$nombre), function (x) edad_en_dias(nombre = tabla_error$nombre[[x]],
+                                                                               edad_en_anos = tabla_error$edad[[x]]))
+
+tabla_final2 <- rbindlist(resultados2)
+
+### Load data ---------------------------------------------------------------------------------------------
+
+
+# Clean up environment ----------------------------------------------------------------------------------
