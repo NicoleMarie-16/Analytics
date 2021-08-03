3d031cd9924fcac037c011ad0a82545fe1a74429
NicoleMarie-16 <nicolemdavila16@gmail.com>
2021-08-02 21:08:56 -0500

    includes assignment for loops, functions, and lapply

---
+++ r_lessons_spanish/asignacion_6.R
@@ -0,0 +1,45 @@
+# Script info ---------------------------------------------------------------------------------------------
+# Script Name: asignacion_6.R 
+# Script Author: Nicole Davila
+# Created date: Mon Aug 02 20:53:34 2021
+# Purpose: Este codigo es una asignacion de funciones, loops y lapply
+
+### Import required libraries -----------------------------------------------------------------------------
+library(data.table)
+
+### Define functions --------------------------------------------------------------------------------------
+# Crea una función que calcule el precio final de un producto basado en el precio del producto
+# y el por ciento de impuestos. Por ejemplo algo que cuesta $1.65 con un impuesto de 10% saldria
+# en $1.82. La funcion debe tomar dos argumentos numericos: el precio y el porciento. Asegúrate de 
+# incluir el tryCatch y regresar el precio final (no tienes que regresar una tabla).
+
+### Define variables --------------------------------------------------------------------------------------
+# Crea un vector con 5 precios. Llamalo "precios"
+
+# Crea una tabla que tenga 5 productos, 5 precios y 5 por cientos de impuestos (3 columnas)
+
+
+### Load data ---------------------------------------------------------------------------------------------
+
+
+### Pre-process -------------------------------------------------------------------------------------------
+
+
+### Process -----------------------------------------------------------------------------------------------
+# Aplica la funcion con los valores $20.50 y 7% de impuestos
+
+
+# Aplica la funcion con un numero y un character. Que error sale?
+
+# Corre la funcion con los valores que desees, pero guarda el resultado en una nueva variable llamada
+# "resultado"
+
+# Escribe un for loop que calcule los precios luego de aplicarle el 10% de impuestos con el vector "precios"
+
+# Utiliza un lapply para calcular el prociento de los productos utilizando la tabla que creaste 
+# anteriormente y aplicando la funcion que escribiste al inicio
+
+### Load data ---------------------------------------------------------------------------------------------
+
+
+# Clean up environment ----------------------------------------------------------------------------------
